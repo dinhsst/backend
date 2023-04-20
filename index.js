aeac10451d09ac0f8eac40fabc072c67dc1c722e
Nguyen Van Dinh <dinhsst@gmail.com>
2023-04-20 23:21:06 +0700

    test

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const tinmoi = require('./routes/tinmoi.route'); // Imports routes for the products
 const app = express();
 app.use('/', tinmoi);
-let port = 1234;
+let port = process.env.PORT | 3000;
 
 app.listen(port, () => {
     console.log('Server is up and running on port numner ' + port);
